1e607d71607d7a1d0f1448cc6e4af20ad17c89c7
Oracle_BH <38717741+oraclBH@users.noreply.github.com>
2022-05-17 13:34:37 +0800

    Update testGIF.m

--- testGIF.m
+++ testGIF.m
@@ -12,17 +12,17 @@ obj_height = obj.Height;
 obj_Width = obj.Width;   
 
  
-len = 400;
+len = 100;
 imageArray = zeros(obj_height, obj_Width, 3, len);
 
-for k = 1 : 400     
+for k = 1 : len     
      newFrame = read(obj, k);  
      imageArray(:,:,:,k) = newFrame;
 end
  
 GifName = 'medicalIMG.gif';
 delay = 0.05;       % Delay between frames (s)
-for ii = 1:400
+for ii = 1:len
     A = uint8(imageArray(:,:,:,ii));
     [X, map] = rgb2ind(A, 256);
     if ii == 1
